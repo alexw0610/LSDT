5336e324fbbd1b070d39175a0fae894529e1ddd5
alexw0610 <alex@mailbag.at>
2020-10-29 17:31:57 +0100

    added maven junit integration

---
+++ src/test/java/TestJunit.java
@@ -0,0 +1,27 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class TestJunit {
+
+    @Before
+    public void setUp(){
+        
+    }
+    @After
+    public void tearDown(){
+
+
+    }
+
+    @Test
+    public void test(){
+        String goal = "Junit";
+        assertEquals("Junit",goal);
+    }
+
+
+}
